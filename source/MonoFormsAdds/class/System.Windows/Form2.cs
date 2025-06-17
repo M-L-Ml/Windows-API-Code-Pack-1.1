8985a9adda9dc2aeecddf78acb2979f16f57e3eb
M-L-Ml <m.l.malinouski@gmail.com>
2025-06-17 13:00:25 +0200

    Fixing non displayed Button text workarounf : tooltip #2: Form2

--- source/MonoFormsAdds/class/System.Windows/Form2.cs
+++ source/MonoFormsAdds/class/System.Windows/Form2.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,20 +15,73 @@ namespace System.Windows.Forms
         public Form2()
         {
         }
-
+        //   public IContainer ComponentsContainer => ;
         protected override void OnHandleCreated(EventArgs e)
         {
-            this.AddTooltipsAndIconsToButtons();
+            var toolTips = this.GetToolTipComponents().ToList();
+            this.AddTooltipsAndIconsToButtons(toolTip: toolTips.FirstOrDefault());
             base.OnHandleCreated(e);
+
         }
     }
 
     public static class ControlExtensions2
     {
+
+        //public static void GetAllToolTips(this Control frm)
+        //{
+        //    IEnumerable<ToolTip> toolTips = frm.GetToolTipComponents();
+        //    var toolTipList = toolTips.ToList();
+        //    if (toolTipList.Count > 0)
+        //    {
+        //        ToolTip tt = ToolTipList[0];
+        //        foreach (Control c in frm.Controls)
+        //        {
+        //            string text = tt.GetToolTip(c);
+
+        //        }
+        //    }
+
+        //}
+
+        public static IEnumerable<ToolTip> GetToolTipComponents(this Control frm)
+        {
+            IContainer parent = GetComponentsContainer(frm);
+            var ToolTipList = parent.Components.OfType<ToolTip>();
+            return ToolTipList;
+        }
+
+        public static IContainer? GetComponentsContainer(this Control frm)
+        {
+            if (frm == null)
+            {
+                throw new ArgumentNullException(nameof(frm), "Control cannot be null.");
+            }
+            var r = frm.Container;
+            if (r != null)
+            {
+                return r;
+            }
+            Type typeForm = frm.GetType();
+            FieldInfo fieldInfo = typeForm.GetField("components", BindingFlags.Instance | BindingFlags.NonPublic);
+            IContainer? parent = (IContainer?)fieldInfo.GetValue(frm);
+            return parent;
+        }
+
         public static int AddTooltipsAndIconsToButtons(this Control parent, Image? image = null, ToolTip? toolTip = null)
         {
-            toolTip = toolTip ?? new ToolTip() { AutomaticDelay = 50, AutoPopDelay = 4000 };
-
+            bool toolTCreated;
+            if (toolTip == null)
+            {
+                toolTip =  new ToolTip() { AutomaticDelay = 50, AutoPopDelay = 4000 };
+                toolTCreated = true;
+            }
+            else
+            {
+                toolTip.AutomaticDelay = 50;
+                toolTip.AutoPopDelay = 4000;
+                toolTCreated = false;
+            }
             //image = image ?? Properties.Resources.Main; // Replace with your desired icon
             int numButtons = 0;
 
@@ -35,18 +90,27 @@ namespace System.Windows.Forms
             {
                 if (control is ButtonBase button)
                 {
-                    toolTip.SetToolTip(button, $" {button.Text}");
-                    button.TextChanged += (o, e) =>
+                    if (!toolTCreated)
                     {
-
-                        if (o is ButtonBase b)
+                        var existing = toolTip.GetToolTip(button);
+                        if (string.IsNullOrEmpty(existing))
                         {
-                            toolTip.SetToolTip(b, $" {b.Text}");
+
+                            toolTip.SetToolTip(button, $"{button.Text} .");
+                            button.TextChanged += (o, e) =>
+                            {
+                                if (o is ButtonBase b)
+                                {
+                                    toolTip.SetToolTip(b, $"{b.Text} .");
+                                }
+                            };
                         }
-                    };
+                    }
                     if (image != null)
+                    {
                         button.Image = image;
-                    button.ImageAlign = ContentAlignment.MiddleLeft;
+                        button.ImageAlign = ContentAlignment.MiddleLeft;
+                    }
                     numButtons++;
                 }
                 else
