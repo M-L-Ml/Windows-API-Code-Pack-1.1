a43d15df53e05c49a0f50557a2e734b17ddb4d94
M-L-Ml <m.l.malinouski@gmail.com>
2025-06-17 21:32:15 +0200

    Fixing non displayed Button text workaround : tooltip #4: Form2

--- source/MonoFormsAdds/class/System.Windows/Form2.cs
+++ source/MonoFormsAdds/class/System.Windows/Form2.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace System.Windows.Forms
 {
@@ -37,28 +38,12 @@ namespace System.Windows.Forms
     public static class ControlExtensions2
     {
 
-        //public static void GetAllToolTips(this Control frm)
-        //{
-        //    IEnumerable<ToolTip> toolTips = frm.GetToolTipComponents();
-        //    var toolTipList = toolTips.ToList();
-        //    if (toolTipList.Count > 0)
-        //    {
-        //        ToolTip tt = toolTips[0];
-        //        foreach (Control c in frm.Controls)
-        //        {
-        //            string text = tt.GetToolTip(c);
-
-        //        }
-        //    }
-
-        //}
-
         public static IEnumerable<ToolTip> GetToolTipComponents(this Control frm)
         {
-            IContainer parent = GetComponentsContainer(frm);
+            IContainer? parent = GetComponentsContainer(frm);
 
-            var toolTips = parent.Components.OfType<ToolTip>();
-            return toolTips;
+            var toolTips = parent?.Components.OfType<ToolTip>();
+            return toolTips ?? Enumerable.Empty<ToolTip>();
         }
 
         public static IContainer? GetComponentsContainer(this Control frm)
@@ -70,66 +55,128 @@ namespace System.Windows.Forms
             var r = frm.Container;
             if (r != null)
             {
+                //never happens, but just in case
                 return r;
             }
 
 
             Type typeForm = frm.GetType();
+            // "components" is private field in Form , default name used by Form designer tools, it's private so we need to use reflection
             FieldInfo fieldInfo = typeForm.GetField("components", BindingFlags.Instance | BindingFlags.NonPublic);
             IContainer? parent = (IContainer?)fieldInfo.GetValue(frm);
             return parent;
         }
 
+        public interface ITraverseCommand
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="control"></param>
+            /// <returns>control was changed</returns>
+            bool Execute(Control control);
+            void InitForParent(Control parent);
+        }
+
+        public class AddTooltipsAndIconsToButtonsCommand : ITraverseCommand
+        {
+            public Image? Image { get; init; }
+            public ToolTip? ToolTip { get; set; }
+            //  public ToolTip? ToolTip { get; init; }
+            public bool Execute(Control control)
+            {
+                if (control is not ButtonBase button)
+                {
+                    return false;
+                }
+                bool changed = false;
+                string? existing = ToolTCreated ? null : ToolTip.GetToolTip(button);
+                if (string.IsNullOrEmpty(existing))
+                {
+                    ToolTip.SetToolTip(button, $"{button.Text} .");
+
+                    changed = true;
+                    button.TextChanged += (o, e) =>
+                    {
+                        if (o is ButtonBase b)
+                        {
+                            ToolTip.SetToolTip(b, $"{b.Text} .");
+                        }
+                    };
+                }
+
+                if (Image != null)
+                {
+                    button.Image = Image;
+                    button.ImageAlign = ContentAlignment.MiddleLeft;
+                    changed = true;
+                }
+                return changed;
+            }
+            bool ToolTCreated = false;
+
+            public void InitForParent(Control parent)
+            {
+                var existing = parent.GetToolTipComponents().FirstOrDefault();
+                if (existing == null && ToolTip == null)
+                {
+                    ToolTip = new ToolTip() { };
+                    ToolTCreated = true;
+                    SetToolTipFastTiming(ToolTip!);
+                    return;
+                }
+                if (existing != null)
+                {
+                    SetToolTipFastTiming(existing);
+                    ToolTip = existing;
+                    //if we have a toolTip, we can set the timing
+                }
+
+                ToolTCreated = false;
+
+            }
+        }
+
+
         public static int AddTooltipsAndIconsToButtons(this Control parent, Image? image = null, ToolTip? toolTip = null)
         {
-            bool toolTCreated;
-            if (toolTip == null)
-            {
-                toolTip = new ToolTip() { AutomaticDelay = 50, AutoPopDelay = 4000 };
-                toolTCreated = true;
-            }
-            else
-            {
-                toolTip.AutomaticDelay = 50;
-                toolTip.AutoPopDelay = 4000;
-                toolTCreated = false;
-            }
-            //image = image ?? Properties.Resources.Main; // Replace with your desired icon
-            int numButtons = 0;
 
 
+            var command = new AddTooltipsAndIconsToButtonsCommand() { Image = image, ToolTip = toolTip };
+            return TraverseControls(parent, command);
+        }
+
+        public static void SetToolTipFastTiming(this ToolTip toolTip)
+        {
+            toolTip.AutomaticDelay = 50;
+            toolTip.AutoPopDelay = 4000;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="command"></param>
+        /// <returns>affectedControls</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static int TraverseControls(this Control parent, ITraverseCommand command)
+        {
+            int affectedControls = 0;
+            if (parent == null)
+            {
+                throw new ArgumentNullException(nameof(parent), "Parent control cannot be null.");
+            }
+            command.InitForParent(parent);
             foreach (Control control in parent.Controls)
             {
-                if (control is ButtonBase button)
+                command.Execute(control);
+                affectedControls++;
+                if (control.Controls.Count > 0)
                 {
-
-                    var existing = toolTCreated ? null : toolTip.GetToolTip(button);
-                    if (string.IsNullOrEmpty(existing))
-                    {
-
-                        toolTip.SetToolTip(button, $"{button.Text} .");
-                        button.TextChanged += (o, e) =>
-                        {
-                            if (o is ButtonBase b)
-                            {
-                                toolTip.SetToolTip(b, $"{b.Text} .");
-                            }
-                        };
-                    }
-
-                    if (image != null)
-                    {
-                        button.Image = image;
-                        button.ImageAlign = ContentAlignment.MiddleLeft;
-                    }
-                    numButtons++;
-                }
-                else
-                {
-                    numButtons += AddTooltipsAndIconsToButtons(control, image, toolTip); // Recursively check child controls
+                    affectedControls += TraverseControls(control, command);
                 }
             }
-            return numButtons;
+            return affectedControls;
         }
     }
 }
